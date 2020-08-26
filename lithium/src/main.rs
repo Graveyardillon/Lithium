671829d7b1ccd08029ac65167ac7724cb559d911
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2020-08-26 12:29:40 +0900

    rustでffmpegを呼び出してコーデック変換

--- lithium/src/main.rs
+++ lithium/src/main.rs
@@ -1,3 +1,26 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, World!");
+    let cmd = format!("ffmpeg");
+
+    let output = Command::new(cmd)
+        .current_dir("./assets")
+        .arg("-i")
+        .arg("sample.ts")
+        .arg("-vcodec")
+        .arg("mpeg2video")
+        .arg("output.ts")
+        .output()
+        .expect("failed to run ffmpeg");
+
+    println!("output: {}", String::from_utf8_lossy(&output.stdout));
+
+    // let cmd = format!("ls");
+
+    // let output = Command::new(cmd)
+    //     .current_dir("./assets")
+    //     .output()
+    //     .expect("failed to run ls");
+
+    // println!("{}", String::from_utf8_lossy(&output.stdout));
 }
